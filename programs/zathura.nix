03751e514f2e1b755efcdeea7d9b912b9aac07ed
koen1859 <koenstevens1859@gmail.com>
2025-03-23 16:17:44 +0100

    Better zathura theme

--- programs/zathura.nix
+++ programs/zathura.nix
@@ -5,7 +5,15 @@
     enable = true;
     options = {
       default-bg = "#${config.lib.stylix.colors.base00}";
-      default-fg = "#${config.lib.stylix.colors.base00}";
+      default-fg = "#${config.lib.stylix.colors.base07}";
+      inputbar-bg = "#${config.lib.stylix.colors.base00}";
+      inputbar-fg = "#${config.lib.stylix.colors.base07}";
+      statusbar-bg = "#${config.lib.stylix.colors.base00}";
+      statusbar-fg = "#${config.lib.stylix.colors.base07}";
+      notification-bg = "#${config.lib.stylix.colors.base00}";
+      notification-fg = "#${config.lib.stylix.colors.base07}";
+      highlight-active-color = "#${config.lib.stylix.colors.base06}50";
+      highlight-color = "#${config.lib.stylix.colors.base04}50";
       recolor = true;
       recolor-lightcolor = "#${config.lib.stylix.colors.base00}";
       recolor-darkcolor = "#${config.lib.stylix.colors.base07}";
