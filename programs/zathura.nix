fc6e7e6a8fed10a5807ce74e5d2fd334fdc1dc8b
koen1859 <koenstevens1859@gmail.com>
2025-03-23 12:00:22 +0100

    zathura theming

--- programs/zathura.nix
+++ programs/zathura.nix
@@ -4,7 +4,11 @@
   programs.zathura = {
     enable = true;
     options = {
-      default-bg = "#${config.lib.stylix.colors.base05}";
+      default-bg = "#${config.lib.stylix.colors.base00}";
+      default-fg = "#${config.lib.stylix.colors.base00}";
+      recolor = true;
+      recolor-lightcolor = "#${config.lib.stylix.colors.base00}";
+      recolor-darkcolor = "#${config.lib.stylix.colors.base07}";
     };
   };
 }
