931e39b5f110eb10bcbbb89e8076e587b62f6fae
koen1859 <koenstevens1859@gmail.com>
2025-04-01 20:51:36 +0200

    zellij cfg

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -36,6 +36,16 @@
         bind "Alt Shift z" {
           MessagePlugin "autolock" {payload "enable";};
         }
+        bind "Alt 1" { GoToTab 1; }
+        bind "Alt 2" { GoToTab 2; }
+        bind "Alt 3" { GoToTab 3; }
+        bind "Alt 4" { GoToTab 4; }
+        bind "Alt 5" { GoToTab 5; }
+        bind "Alt 6" { GoToTab 6; }
+        bind "Alt 7" { GoToTab 7; }
+        bind "Alt 8" { GoToTab 8; }
+        bind "Alt 9" { GoToTab 9; }
+
       }
       shared_except "locked" {
         bind "Alt z" {
@@ -52,15 +62,6 @@
         bind "Alt q" { CloseFocus; }
         bind "Alt c" { Clear; }
         bind "Alt d" { Detach; }
-        bind "Alt 1" { GoToTab 1; }
-        bind "Alt 2" { GoToTab 2; }
-        bind "Alt 3" { GoToTab 3; }
-        bind "Alt 4" { GoToTab 4; }
-        bind "Alt 5" { GoToTab 5; }
-        bind "Alt 6" { GoToTab 6; }
-        bind "Alt 7" { GoToTab 7; }
-        bind "Alt 8" { GoToTab 8; }
-        bind "Alt 9" { GoToTab 9; }
       }
     }
     plugins {
