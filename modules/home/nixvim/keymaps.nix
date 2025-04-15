76b90d838f536e83125b78f178a9fc7ca33adac1
koen1859 <koenstevens1859@gmail.com>
2025-04-15 09:33:34 +0200

    Some new vim keybinds

--- modules/home/nixvim/keymaps.nix
+++ modules/home/nixvim/keymaps.nix
@@ -2,15 +2,60 @@
   programs.nixvim = {
     keymaps = [
       {
+        key = "<Up>";
+        mode = ["n"];
+        action = ":resize -2<CR>";
+      }
+      {
+        key = "<Down>";
+        mode = ["n"];
+        action = ":resize +2<CR>";
+      }
+      {
+        key = "<Left>";
+        mode = ["n"];
+        action = ":vertical resize -2<CR>";
+      }
+      {
+        key = "<Right>";
+        mode = ["n"];
+        action = ":vertical resize +2<CR>";
+      }
+      {
+        key = "<leader>ldt";
+        mode = ["n"];
+        action = ":lua vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })<CR>";
+      }
+      {
         key = "<leader>c";
         mode = ["n"];
         options.silent = true;
         action = ":w<CR>:!ls % | entr -n bash ~/.dotfiles/modules/home/nixvim/build-pandoc.sh % &<CR>";
       }
       {
+        key = "<leader><S-y>";
+        mode = ["n" "v"];
+        action = "\"+Y";
+      }
+      {
+        key = "<leader>y";
+        mode = ["n" "v"];
+        action = "\"+y";
+      }
+      {
         key = "gd";
         mode = ["n"];
         action = "<cmd>Telescope lsp_definitions<CR>";
+      }
+      {
+        key = "gr";
+        mode = ["n"];
+        action = "<cmd>Telescope lsp_references<CR>";
+      }
+      {
+        key = "gi";
+        mode = ["n"];
+        action = "<cmd>Telescope lsp_implementations<CR>";
       }
       {
         mode = "n";
--- modules/home/nixvim/misc.nix
+++ modules/home/nixvim/misc.nix
@@ -2,5 +2,6 @@
   programs.nixvim.plugins = {
     nvim-surround.enable = true;
     nvim-autopairs.enable = true;
+    treesj.enable = true;
   };
 }
