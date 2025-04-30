3668baa667d469be00b167513732a1e90e15d6db
koen1859 <koenstevens1859@gmail.com>
2025-04-30 16:40:39 +0200

    cleanup

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -6,17 +6,6 @@
       pulse.enable = true;
     };
     libinput.enable = true;
-    # postgresql = {
-    #   enable = true;
-    #   package = pkgs.postgresql_17;
-    #   dataDir = "/var/lib/postgresql/data";
-    #   extensions = with pkgs.postgresql17Packages; [postgis];
-    #   authentication = ''
-    #     local   all             all                                     trust
-    #     host    all             all             127.0.0.1/32            trust
-    #     host    all             all             ::1/128                 trust
-    #   '';
-    # };
     flatpak = {
       enable = true;
     };
@@ -30,6 +19,5 @@
         };
       };
     };
-    # services.printing.enable = true;
   };
 }
