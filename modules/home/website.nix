4ccfbe8501e0e52c6ae43d50558ea5dc5da8b6d0
koen1859 <koenstevens1859@gmail.com>
2025-05-03 18:01:16 +0200

    website deploy script

--- modules/home/website.nix
+++ modules/home/website.nix
@@ -2,5 +2,5 @@
 pkgs.writeShellScriptBin "deploy-website" ''
   rsync -avz --delete ~/website/* VPS:~/website/
   ssh VPS "sudo rm -r /var/www/html/*"
-  ssh VPS "sudo cp -r ~/website/ /var/www/html/"
+  ssh VPS "sudo cp -r ~/website/* /var/www/html/"
 ''
