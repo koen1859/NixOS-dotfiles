56451879c1ec44c6584c2776d637a95d8a3b4d2f
koen1859 <koenstevens1859@gmail.com>
2025-05-05 13:19:11 +0200

    test

--- modules/home/website.nix
+++ modules/home/website.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "deploy-website" ''
+  cd ~/website
+  git add -A
+  git commit -m "Deploy Website"
+  git push github main
   ssh VPS "cd ~/website && git pull github main"
   ssh VPS "sudo rsync -a --delete ~/website/ /var/www/html/"
 ''
