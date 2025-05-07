5d6a8b59b3f0556deda1c344b50f6dfa6b145a8b
koen1859 <koenstevens1859@gmail.com>
2025-05-07 19:32:20 +0200

    safer website

--- modules/home/website.nix
+++ modules/home/website.nix
@@ -5,5 +5,5 @@ pkgs.writeShellScriptBin "deploy-website" ''
   git commit -m "Deploy Website"
   git push github main
   ssh VPS "cd ~/website && git pull github main"
-  ssh VPS "sudo rsync -a --delete ~/website/ /var/www/html/"
+  ssh VPS "sudo rsync -a --delete --exclude='.git' ~/website/ /var/www/html/"
 ''
