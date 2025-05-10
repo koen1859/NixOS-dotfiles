44b7a6a7ca0f93c1115e9e6a3d071c5696d32dae
koen1859 <koenstevens1859@gmail.com>
2025-05-10 09:53:52 +0200

    Better VPN script

--- modules/home/scripts/VPN_Toggle.nix
+++ modules/home/scripts/VPN_Toggle.nix
@@ -1,26 +1,31 @@
 {pkgs, ...}:
-pkgs.writeShellScriptBin "VPN_Toggle" ''
+pkgs.writeShellScriptBin "VPN_TOGGLE" ''
   set -e
 
   VPN_DIR=~/VPN
   CONF_FILES=($VPN_DIR/*.conf)
+  WG_QUICK=${pkgs.wireguard-tools}/bin/wg-quick
 
-  echo "Select a VPN config:"
+  # Check if any interface is currently up
+  for CONF_PATH in "''${CONF_FILES[@]}"; do
+    INTERFACE=$(basename "$CONF_PATH" .conf)
+    if ip link show "$INTERFACE" &> /dev/null; then
+      echo "VPN $INTERFACE is currently active. Bringing it down..."
+      sudo $WG_QUICK down "$CONF_PATH"
+      exit 0
+    fi
+  done
+
+  # If no VPN is up, let user choose one to connect
+  echo "Select a VPN config to connect:"
   select CONFIG_PATH in "''${CONF_FILES[@]}"; do
     if [[ -n "$CONFIG_PATH" ]]; then
+      INTERFACE=$(basename "$CONFIG_PATH" .conf)
+      echo "Connecting to $INTERFACE..."
+      sudo $WG_QUICK up "$CONFIG_PATH"
       break
     else
       echo "Invalid selection."
     fi
   done
-
-  INTERFACE=$(basename "$CONFIG_PATH" .conf)
-
-  if ip link show "$INTERFACE" &> /dev/null; then
-    echo "Bringing down $INTERFACE"
-    sudo ${pkgs.wireguard-tools}/bin/wg-quick down "$CONFIG_PATH"
-  else
-    echo "Bringing up $INTERFACE"
-    sudo ${pkgs.wireguard-tools}/bin/wg-quick up "$CONFIG_PATH"
-  fi
 ''
