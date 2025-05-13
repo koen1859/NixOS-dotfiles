b14bc3a8678a3c80419629471276cb2f0db97570
koen1859 <koenstevens1859@gmail.com>
2025-05-13 22:19:28 +0200

    fonts

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
+    nerd-fonts.code-new-roman
     nerd-fonts.jetbrains-mono
+    nerd-fonts.caskaydia-cove
     nerd-fonts.symbols-only
     montserrat
   ];
