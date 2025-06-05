525ed8e6f8e81e139d886689d4d148fc4840e41a
koen1859 <koenstevens1859@gmail.com>
2025-06-05 16:56:20 +0200

    colors

--- modules/theme.nix
+++ modules/theme.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
-  # theme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  theme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   # theme = "${pkgs.base16-schemes}/share/themes/horizon-dark.yaml";
   # theme = "${pkgs.base16-schemes}/share/themes/spacemacs.yaml";
-  theme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+  # theme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
 }
