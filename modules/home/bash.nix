8469115517dbe6ca735c6a6462c8eb05f7a95255
koen1859 <koenstevens1859@gmail.com>
2025-06-08 12:57:52 +0200

    define blesh package

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -1,4 +1,32 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  ble-sh = pkgs.stdenv.mkDerivation rec {
+    pname = "ble.sh";
+    version = "nightly-2024-06";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz";
+      sha256 = "13qls3c1gy0jsjqpg4ja8y5im7mj5wk03p4cpq9x3lv713aa6im7";
+    };
+    nativeBuildInputs = [pkgs.xz];
+
+    unpackPhase = ''
+      mkdir source
+      tar -xJf ${src} -C source --strip-components=1
+    '';
+
+    installPhase = ''
+      mkdir -p $out/share/blesh
+      cp -r . $out/share/blesh
+    '';
+
+    meta = {
+      description = "Bash Line Editor (ble.sh) - nightly release";
+      homepage = "https://github.com/akinomyoga/ble.sh";
+      license = pkgs.lib.licenses.mit;
+    };
+  };
+in {
+  home.packages = [ble-sh];
   home.sessionVariables = {
     EDITOR = "nvim";
     XDG_CONFIG_HOME = "$HOME/.config";
@@ -19,7 +47,7 @@
         gc = "git commit";
       };
       initExtra = ''
-        source ~/.local/share/blesh/ble.sh
+        source ${ble-sh}/share/blesh/source/ble.sh
       '';
     };
   };
