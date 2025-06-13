f51098f7380317e39353801f5dc8e886e3e85a8e
koen1859 <koenstevens1859@gmail.com>
2025-06-13 19:46:15 +0200

    fix

--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -7,6 +7,11 @@
   };
   xdg.portal = {
     enable = true;
+    config = {
+      common = {
+        default = ["hyprland"];
+      };
+    };
     extraPortals = [pkgs.xdg-desktop-portal-hyprland];
   };
 }
