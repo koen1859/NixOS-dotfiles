5628b0eead0fb14b39cbc1d75f57776edba7af34
koen1859 <koenstevens1859@gmail.com>
2025-06-19 17:33:04 +0200

    brave

--- modules/home/brave.nix
+++ modules/home/brave.nix
@@ -4,12 +4,11 @@
       # {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock origin
       {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} # vimium
       # {id = "fihnjjcciajhdojfnbdddfaoknhalnja";} # idc ab cookies
-      # {id = "ghmbeldphafepmbegfdlkpapadhbakde";} # proton pass
-      # {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # sponsorblock yt
-      # {id = "jiaopdjbehhjgokpphdfgmapkobbnmjp";} # shorts block yt
+      {id = "ghmbeldphafepmbegfdlkpapadhbakde";} # proton pass
+      {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # sponsorblock yt
+      {id = "jiaopdjbehhjgokpphdfgmapkobbnmjp";} # shorts block yt
       # {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # dark reader
-      # {id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp";} # privacy badger
-      # {id = "fehfojhhnbfbclgpffmffigfgngbpnmj";} # vertical tabs
+      {id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp";} # privacy badger
     ];
   };
 }
