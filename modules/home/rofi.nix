05c4ed4e272f4a6ab43f48a01295be07f29a0c4a
koenstevens <gitlab.slurp563@passmail.net>
2025-08-17 17:55:24 +0200

    cleanup

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -34,45 +34,15 @@ in {
         background-color = mkLiteral "transparent";
         foreground = mkLiteral "${base0D}";
         text-color = mkLiteral "${base0D}";
-        padding = mkLiteral "0px";
-        margin = mkLiteral "0px";
       };
 
       window = {
-        fullscreen = true;
-        padding = mkLiteral "1em";
         background-color = mkLiteral "${base00}dd";
-      };
-
-      mainbox = {
-        padding = mkLiteral "8px";
       };
 
       inputbar = {
         background-color = mkLiteral "${base0D}20";
-
-        margin = mkLiteral "0px calc( 50% - 230px )";
-        padding = mkLiteral "4px 8px";
-        spacing = mkLiteral "8px";
-
-        border = mkLiteral "1px";
-        border-radius = mkLiteral "2px";
         border-color = mkLiteral "${base0D}40";
-
-        children = map mkLiteral [
-          "icon-search"
-          "entry"
-        ];
-      };
-
-      prompt = {
-        enabled = false;
-      };
-
-      icon-search = {
-        expand = false;
-        filename = "search";
-        vertical-align = mkLiteral "0.5";
       };
 
       entry = {
@@ -80,37 +50,8 @@ in {
         placeholder-color = mkLiteral "${base0D}20";
       };
 
-      listview = {
-        margin = mkLiteral "48px calc( 50% - 720px )";
-        margin-bottom = mkLiteral "0px";
-        spacing = mkLiteral "48px";
-        columns = 6;
-        fixed-columns = true;
-      };
-
-      "element, element-text, element-icon" = {
-        cursor = mkLiteral "pointer";
-      };
-
-      element = {
-        padding = mkLiteral "8px";
-        spacing = mkLiteral "4px";
-
-        orientation = mkLiteral "vertical";
-        border-radius = mkLiteral "12px";
-      };
-
       "element selected" = {
         background-color = mkLiteral "${base0D}33";
-      };
-
-      element-icon = {
-        size = mkLiteral "5.75em";
-        horizontal-align = mkLiteral "0.5";
-      };
-
-      element-text = {
-        horizontal-align = mkLiteral "0.5";
       };
     };
   };
