8918cee6592613cccf9e78d6d53794b43ac8a464
koenstevens <gitlab.slurp563@passmail.net>
2025-09-07 22:58:39 +0200

    new wallpaper

--- variables.nix
+++ variables.nix
@@ -5,7 +5,7 @@
 }: {
   username = "koenstevens";
   theme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  wallpaper = ./Wallpapers/a_house_in_the_woods.png;
+  wallpaper = ./Wallpapers/solar_system.png;
   core = "${inputs.self}/modules/core";
   home = "${inputs.self}/modules/home";
   shell = pkgs.zsh;
