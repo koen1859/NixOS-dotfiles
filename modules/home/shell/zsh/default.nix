a8582c24a0dc670a7b67e6b3f25107975a685ec3
koen1859 <mail@koenstevens.nl>
2025-09-11 17:30:04 +0200

    zsh

--- modules/home/shell/zsh/default.nix
+++ modules/home/shell/zsh/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
@@ -9,6 +13,16 @@
     };
 
     plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
     ];
 
     initContent = ''
