d3e459ab2aa97d3a272d614bfecbf777af534480
koenstevens <gitlab.slurp563@passmail.net>
2025-09-18 16:11:01 +0200

    dwm

--- modules/core/dwm/config.h
+++ modules/core/dwm/config.h
@@ -1,7 +1,4 @@
-/* See LICENSE file for copyright and license details. */
-
-/* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int borderpx = 0; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
@@ -18,7 +15,6 @@ static const char *colors[][3] = {
     [SchemeSel] = {col_gray4, col_cyan, col_cyan},
 };
 
-/* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
@@ -27,8 +23,8 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+    // {"Gimp", NULL, NULL, 0, 1, -1},
+    // {"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
 
 /* layout(s) */
@@ -66,15 +62,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",    col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4};
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
-static const char *browsercmd[] = {"firefox"};
-static const char *privatebrowsercmd[] = {"brave", "--incognito"};
-static const char *protonvpncmd[] = {"protonvpn"};
-static const char *protonpasscmd[] = {"proton-pass"};
-static const char *protonmailcmd[] = {"proton-mail"};
-static const char *wlogoutcmd[] = {"wlogout"};
+static const char *browsercmd[] = {"firefox", NULL};
+static const char *privatebrowsercmd[] = {"brave", "--incognito", NULL};
+static const char *protonvpncmd[] = {"protonvpn", NULL};
+static const char *protonpasscmd[] = {"proton-pass", NULL};
+static const char *protonmailcmd[] = {"proton-mail", NULL};
+static const char *wlogoutcmd[] = {"wlogout", NULL};
 static const char *brightness_up[] = {"brightnessctl", "set", "1%+"};
 static const char *brightness_down[] = {"brightnessctl", "set", "1%-"};
 static const char *brightness_max[] = {"brightnessctl", "set", "100%"};
@@ -86,6 +82,9 @@ static const char *volume_up[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@",
 static const char *volume_down[] = {"wpctl", "set-volume",
                                     "@DEFAULT_AUDIO_SINK@", "5%-"};
 static const char *filemgrcmd[] = {"rofi", "-show", "filebrowser"};
+static const char *bookcmd[] = {
+    "sh", "-c", "ls",      "~/Documents/Books",   "|", "dmenu", "|", "xargs",
+    "-I", "{}", "zathura", "~/Documents/Books/{}"};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
@@ -93,6 +92,7 @@ static const Key keys[] = {
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, spawn, {.v = browsercmd}},
     {MODKEY, XK_e, spawn, {.v = filemgrcmd}},
+    {MODKEY, XK_o, spawn, {.v = bookcmd}},
     {MODKEY, XK_p, spawn, {.v = protonpasscmd}},
     {MODKEY, XK_m, spawn, {.v = protonmailcmd}},
     {MODKEY | ShiftMask, XK_b, spawn, {.v = privatebrowsercmd}},
