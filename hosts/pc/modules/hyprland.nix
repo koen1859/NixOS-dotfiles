6e21ca8bdab316a2dac660c9b030e8c136b235aa
koenstevens <gitlab.slurp563@passmail.net>
2025-10-07 21:39:37 +0200

    update

--- hosts/pc/modules/hyprland.nix
+++ hosts/pc/modules/hyprland.nix
@@ -5,5 +5,18 @@
         sensitivity = -1.0;
       };
     };
+    extraConfig = ''
+      workspace=1,monitor:DP-1
+      workspace=2,monitor:DP-1
+      workspace=3,monitor:DP-1
+      workspace=4,monitor:DP-1
+      workspace=5,monitor:DP-1
+
+      workspace=6,monitor:HDMI-A-1,
+      workspace=7,monitor:HDMI-A-1,
+      workspace=8,monitor:HDMI-A-1,
+      workspace=9,monitor:HDMI-A-1,
+      workspace=10,monitor:HDMI-A-1,
+    '';
   };
 }
