06efab2fd96f344aa7b0f5240be0cc093d3a9fcc
koenstevens <gitlab.slurp563@passmail.net>
2025-10-27 09:51:02 +0100

    tlp->power-profiles

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./modules/tlp.nix
+    ./modules/auto-cpufreq.nix
     ./modules/hardware.nix
     ./modules/gaming.nix
   ];
--- hosts/laptop/modules/tlp.nix
+++ hosts/laptop/modules/tlp.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   services.tlp = {
-    enable = true;
+    enable = false;
     package = pkgs.tlp;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
