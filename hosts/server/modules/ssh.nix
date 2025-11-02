e7a1f932343e06e4c2d1006e5196665092a26e39
koen1859 <mail@koenstevens.nl>
2025-11-02 21:40:00 +0100

    Manage openssh secrets with sops

--- hosts/server/modules/ssh.nix
+++ hosts/server/modules/ssh.nix
@@ -1,10 +1,11 @@
-{
+{config, ...}: {
   services = {
     fail2ban = {
       enable = true;
     };
     openssh = {
       enable = true;
+      authorizedKeysFiles = [config.sops.secrets.ssh_public_key.path];
       settings = {
         PasswordAuthentication = false;
         PermitRootLogin = "no";
--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -5,4 +5,9 @@
   sops.secrets."main_domain" = {};
   sops.secrets."search_domain" = {};
   sops.secrets."routing_domain" = {};
+  sops.secrets."ssh_public_key" = {};
+  sops.secrets."ssh_private_key" = {
+    owner = "koenstevens";
+    mode = "0600";
+  };
 }
