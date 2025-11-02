dac70d2ca7def144a738f1092f60c044196dbf49
koenstevens <gitlab.slurp563@passmail.net>
2025-11-02 22:21:31 +0100

    Sops for ssh

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -2,8 +2,5 @@
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
   sops.secrets."searx" = {};
-  sops.secrets."main_domain" = {};
-  sops.secrets."search_domain" = {};
-  sops.secrets."routing_domain" = {};
   sops.secrets."ssh_public_key" = {};
 }
