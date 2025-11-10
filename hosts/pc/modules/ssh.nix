6129c7f1476ed3ce958ee57f3170c96c5d47524f
koenstevens <gitlab.slurp563@passmail.net>
2025-11-11 00:13:01 +0100

    SSH server

--- hosts/pc/modules/ssh.nix
+++ hosts/pc/modules/ssh.nix
@@ -7,7 +7,7 @@
       enable = true;
       authorizedKeysFiles = [config.sops.secrets.ssh_public_key.path];
       settings = {
-        PasswordAuthentication = true;
+        PasswordAuthentication = false;
         PermitRootLogin = "no";
       };
       ports = [21];
--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -1,6 +1,8 @@
-{
+{username, ...}: {
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
   sops.secrets."searx" = {};
-  sops.secrets."ssh_public_key" = {};
+  sops.secrets."ssh_public_key" = {
+    owner = username;
+  };
 }
