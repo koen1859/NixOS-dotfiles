eca79d6a13aa82dc6bf5ca596512b7468a226458
koenstevens <gitlab.slurp563@passmail.net>
2025-11-25 17:17:39 +0100

    rpi testing

--- hosts/rpi5/modules/adguard.nix
+++ hosts/rpi5/modules/adguard.nix
@@ -5,4 +5,14 @@
   };
   networking.firewall.allowedTCPPorts = [53];
   networking.firewall.allowedUDPPorts = [53];
+  services.nginx = {
+    enable = true;
+    virtualHosts."dns.koenstevens.nl" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+      };
+    };
+  };
 }
